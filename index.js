d4a6fd474fab5fd425f16248f1a96ba735c0b682
John Gallagher <gallagherj0913@gmail.com>
2021-11-03 01:54:31 -0600

    adding prompts for intern and engineer

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Employee = require('./lib/Employee');
 
 
 // TODO: Create a function to initialize app
-function getManager() {
+const ManagerPrompt = () => {
 return inquirer.prompt([
     {
         type: "input",
@@ -30,35 +30,23 @@ return inquirer.prompt([
         message: "What is the Manager's office number?",
     }
 ])
-.then(maangerData => {
-    const {name, id, email, officeNumber} = maangerData
-    employee = new Manager(name, id, email, officeNumber)
-    let role = {role: "Manager"}
-    return {...maangerData, ...role}
+.then(managerData => {
+    const {name, id, email, officeNumber} = managerData;
+    employee = new Manager(name, id, email, officeNumber);
+    let role = {role: "Manager"};
+    return {...maangerData, ...role};
 })
 }
 
-function newEngineer() {
-    let newEngineer = new Engineer();
-    if(newEngineer === { }) {
-        newEngineer.getName();
-    } else {
-        addEmployee();
-    }
-}
-
-function newIntern() {
-    let newIntern = new Intern();
-    if(newIntern === {}) {
-        newIntern.getName();
-    } else {
-        addEmployee();
-    }  
-}
-
-function addEmployee() {
+const addEmployee = (managerData) => {
     // Two if statements for managerData.engineersArr and managerData.internsArr
-    inquirer    
+    if(!managerData.engineersArr) {
+        managerData.engineersArr = [];
+    }
+    if(!managerData.internsArr) {
+        managerData.internsArr = [];
+    }
+    return inquirer    
         .prompt([
             {
                 type: 'list',
@@ -69,18 +57,68 @@ function addEmployee() {
         ])
         .then((val) => {
             if(val.choices === "Engineer") {
-                newEngineer();
+                return inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "name",
+                        message: "What is the engineer's name?",
+                    },
+                    {
+                        type: "input",
+                        name: "id",
+                        message: "What is the engineer's id?",
+                    },
+                    {
+                        type: "input",
+                        name: "email",
+                        message: "What is the engineer's email?",
+                    },
+                    {
+                        type: "input",
+                        name: "github",
+                        message: "What is the engineer's github username?",
+                    }
+                ]) .then(engineerData => {
+                    engineerData = new Engineer(engineerData.name, engineerData.id, engineerData.email, engineerData.github);
+                    let role = {role: "Engineer"};
+                    managerData.engineersArr.push({...engineerData,...role});
+                    return addEmployee(managerData);
+                })
                 // After questions emplyee = new Engineer(employeeData.name and so on...) and pass in questions data
                 // Set role = to Engineer
                 // managerData.engineers.push({...employeeData,...role})
                 // return addEmployee till user has selected completed
+            } else if(val.choices === "Intern") {
+                return inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "name",
+                        message: "What is the intern's name?",
+                    },
+                    {
+                        type: "input",
+                        name: "id",
+                        message: "What is the intern's id?",
+                    },
+                    {
+                        type: "input",
+                        name: "email",
+                        message: "What is the intern's email?",
+                    },
+                    {
+                        type: "input",
+                        name: "school",
+                        message: "What school does the intern attend?",
+                    }
+                ]) .then(internData => {
+                    internData = new Intern(internData.name, internData.id, internData.email, internData.school);
+                    let role = {role: "Intern"};
+                    managerData.internsArr.push({...internData,...role});
+                    return addEmployee(managerData);
+                })
+            } else {
+                return managerData;
             }
-            if(val.choices === "Intern") {
-                newIntern();
-            }
-            // } else {
-            //     // call function to generate html
-            // }
             
         })
 }
@@ -88,7 +126,7 @@ function addEmployee() {
 // Need function to generate html
 
 // Function call to initalize app
-getManager();
+ManagerPrompt();
 
 // After Manager is answered Prompt Intern/Engineer questions
 // After Completed is selected Prompt write file 
